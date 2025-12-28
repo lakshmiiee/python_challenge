15510b88f9ecdd6ef4d3bdf75af962d212001ce2
Lakshmi Gireesh <lakshmigireesh1289@gmail.com>
2025-12-28 14:38:10 +0530

    Q3

--- 3.square_root.py
+++ 3.square_root.py
@@ -1,4 +1,11 @@
 num=int(input("Enter a number:"))
-
 root=num ** 0.5
 print("The root of", num ,"is", root)
+
+#for complex number
+
+import cmath
+
+num=eval(input("Enter the number:"))
+sqrt=cmath.sqrt(num)
+print("The square root is:",sqrt)
